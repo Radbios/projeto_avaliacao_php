499986300b60f2d936ef2371595504dcc31373dc
Fabiano <radbios@gmail.com>
2024-06-30 00:09:47 -0300

    feat: new command 'echo' code php

--- projeto_avaliacao_php/app/traits/View.php
+++ projeto_avaliacao_php/app/traits/View.php
@@ -1,16 +1,25 @@
 <?php
 
 namespace app\traits;
+use app\exceptions\ViewNotExistException;
 use DirectoryIterator;
 
 trait View {
 
     private $view_path = '../views/';
-    public function view($view, $data) {  
+    public function view($view, $data) {
+        $data = (object) $data;
+
         $template = $this->view_path . str_replace('.', '/', $view) . '.php';
+
+        if (!file_exists($template)) {
+            throw new ViewNotExistException("View {$template} não encontrado.");
+        }
+        $conteudo = file_get_contents($template);
+        $conteudo = str_replace(["{{", "}}"], ["<?php echo ", " ?>"], $conteudo);
         
         ob_start();
-        include $template;
+        eval('?>' . $conteudo);
         $conteudo = ob_get_clean();
 
         return $conteudo;
